763e690a71a80fe2785600d3589b892cde840401
Tanvi Kawle <darshan.p@nimapinfotech.com>
2024-06-10 16:23:21 +0530

    cron jobs created for practice.

--- error_handeling.js
+++
@@ -1,8 +0,0 @@
-const cron = require('node-cron');
-function logMessage() {
- console.log('Cron job executed at:', new Date().toLocaleString());
-}
-// Schedule the cron job to run every minute
-cron.schedule('* * * * *', () => {
- logMessage();
-});
